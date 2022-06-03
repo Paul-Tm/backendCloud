743d3328f16251a241db1b8e4b22a98a65e7a969
Paul Tm <ptomei44@gmail.com>
2022-06-03 16:12:24 +0200

    testconf

--- src/main/java/petition.java
+++ src/main/java/petition.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Petition {
     public String name;
     public String body;
-    public ArrayList<String>Tags;
+    public ArrayList<String>tags;
+    public Petition() {
+	}
 
 }
